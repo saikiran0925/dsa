af73c5e6db5bc5dd5db774b3963fa959cbb1d4fd
Sai Kiran <saikiranbade2018@gmail.com>
2022-10-06 18:06:06 +0530

    SelectionSort Algorithm

--- SelectionSort.java
+++ SelectionSort.java
@@ -3,9 +3,9 @@ package dsa;
 class SelectionSortAlg {
 	public void toSort(int[] arr) {
 		int n = arr.length;
-		for (int i = 0; i < n - 1; i++) {
+		for (int i = 0; i < n ; i++) {
 			int min = i;
-			for (int j = i + 1; j < n - 1; j++) {
+			for (int j = i + 1; j < n ; j++) {
 				if (arr[j] < arr[min]) {
 					min = j;
 				}
