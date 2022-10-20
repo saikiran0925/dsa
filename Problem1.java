559ddf8ed7d5548a457b850451d5ba44e7caf03e
Sai Kiran <saikiranbade2018@gmail.com>
2022-10-20 16:27:32 +0530

    All problems done

--- Problem1.java
+++ Problem1.java
@@ -5,7 +5,7 @@ public class Problem1 {
 	public static void main(String[] args) {
 		String[] text = { "The patient should be informed of his therapeutic options.",
 				"It wouldn't be as if the lone astronaut would be completely by himself.",
-				"Now, a writer is entitled to have a Roget on his desk.", 
+				"Now, a writer is entitled to have a Roget on his desk.",
 				"Somebody should let you borrow their book",
 				"From the earliest times until about the 1960s",
 				"whether we may not, nay ought not, to use a neutral pronoun relative",
@@ -13,20 +13,33 @@ public class Problem1 {
 				"Notionalplurality", 
 				"A starting point would be to give more support", 
 				"abc@gmail.com" };
-		String[] regex = {
-				".*\\.$",
+		String[] regex = { ".*\\.$",
 				".*\\d{10}.*",
-				".*@.*",
+				".*@.*", 
 				"\\w+",
-				".*\\s.*"
-		};
-		for(int i=0; i<10; i++) {
-			for(int j=0; j<5; j++ ) {
-				 if(text[i].matches(regex[j])) {
-					 System.out.println("String: " + (i+1) +" matches regex: " + (j+1));
-				 }
+				".*\\s.*" };
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 10; j++) {
+				if (text[j].matches(regex[i])) {
+					System.out.println("String: " + (j + 1) + " matches regex: " + (i + 1));
+				}
 			}
 		}
 	}
 
 }
+
+//Algorithm if we want to pass texts and regex as String arrays.
+//class Algorithm{
+//	public void requiredAlg(String[] text, String[] regex) {
+//		int m = text.length;
+//		int n = regex.length;
+//		for(int i=0; i<n; i++) {
+//			for(int j=0; j<m; j++ ) {
+//				 if(text[j].matches(regex[i])) {
+//					 System.out.println("String: " + (j+1) +" matches regex: " + (i+1));
+//				 }
+//			}
+//		}
+//	}
+//}
